225a1b7cbf5b6dc22e1943ae9f0d4984df02622e
Alistair Miles <alimanfoo@googlemail.com>
2011-07-21 16:56:27 +0100

    initial setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-"""
-TODO
+#!/usr/bin/env python
 
-"""
+from distutils.core import setup
+
+setup(name='csvvalidator',
+      version='1.0',
+      author='Alistair Miles',
+      author_email='alimanfoo@googlemail.com',
+      url='https://github.com/alimanfoo/csvvalidator',
+      py_modules=['csvvalidator'],
+      description='A simple library for validating data contained in CSV files or similar row-oriented data sources.',
+      long_description=open('README.txt').read()
+      )
