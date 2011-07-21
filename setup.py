a99af6a80e50dc89e923852b8ce67d3c3c0a70b8
Alistair Miles <alimanfoo@googlemail.com>
2011-07-21 17:21:18 +0100

    added license and classifiers

--- setup.py
+++ setup.py
@@ -7,7 +7,13 @@ setup(name='csvvalidator',
       author='Alistair Miles',
       author_email='alimanfoo@googlemail.com',
       url='https://github.com/alimanfoo/csvvalidator',
+      license='MIT License',
       py_modules=['csvvalidator'],
       description='A simple library for validating data contained in CSV files or similar row-oriented data sources.',
-      long_description=open('README.txt').read()
+      long_description=open('README.txt').read(),
+      classifiers=['Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules'
+                   ]
       )
