50a86eefdc549c48f65a91a5c0a66099010ee65d
Alistair Miles <alimanfoo@googlemail.com>
2013-05-16 14:34:16 +0100

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='csvvalidator',
-      version='1.2',
+      version='1.3-SNAPSHOT',
       author='Alistair Miles',
       author_email='alimanfoo@googlemail.com',
       url='https://github.com/alimanfoo/csvvalidator',
